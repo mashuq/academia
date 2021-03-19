8014bd13f5dc8967a27d687b4f638a331c66af67
Mashuq Rahman <mashuq@gmail.com>
2021-03-19 23:52:20 +0600

    gunicorn conf file

---
+++ backend/gunicorn.conf.py
@@ -0,0 +1,6 @@
+bind = '0.0.0.0:443'
+workers = 8
+threads = 8
+keyfile = '/etc/letsencrypt/live/api.nlquran.com/privkey.pem'
+certfile = '/etc/letsencrypt/live/api.nlquran.com/cert.pem'
+ca_certs = '/etc/letsencrypt/live/api.nlquran.com/chain.pem'
